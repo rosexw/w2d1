3cb209367b862e3f855ec9bba5d0b2779ba36402
Rose Wan <rwan@mail.com>
2017-06-05 16:56:08 +0000

    step 4

--- step4.js
+++ step4.js
@@ -1,13 +1,10 @@
 var https = require('https');
-function getAndPrintHTML (options) {
+function getHTML (options, callback) {
 
   /* Add your code here */
   var stream = "";
-
   https.get(options, function (response) {
-
     response.setEncoding('utf8');
-
     response.on('data', function (data) {
       stream += data;
     });
@@ -15,13 +12,16 @@ function getAndPrintHTML (options) {
     response.on('end', function() {
       console.log('\n buffer', stream);
     });
-
   });
+}
+
+function printHTML (html) {
+  console.log(html);
 }
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step3.html'
+  path: '/http-examples/step4.html'
 };
 
-getAndPrintHTML(requestOptions);
+getHTML (requestOptions, printHTML);
